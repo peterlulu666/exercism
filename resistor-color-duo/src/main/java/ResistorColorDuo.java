bf8d774f46e82f2b21eeed2e34f922304af8c1b6
root <root@ide.ide.9719e147-a351-4b5f-9839-94e474164604.svc.cluster.local>
2019-12-10 23:18:07 +0000

    commit

---
+++ resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -0,0 +1,59 @@
+class ResistorColorDuo {
+    int value(String[] colors) {
+        String code = "";
+        for (int i = 0; i < 2; i++) {
+            code = code + colorCode(colors[i]);
+
+        }
+
+        return Integer.parseInt(code);
+
+    }
+
+    int colorCode(String color) {
+        switch (color) {
+            case "black":
+                return 0;
+
+            case "brown":
+                return 1;
+
+            case "red":
+                return 2;
+
+            case "orange":
+                return 3;
+
+            case "yellow":
+                return 4;
+
+            case "green":
+                return 5;
+
+            case "blue":
+                return 6;
+
+            case "violet":
+                return 7;
+
+            case "grey":
+                return 8;
+
+            case "white":
+                return 9;
+
+        }
+
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        var resistorColorDuo = new ResistorColorDuo();
+        var colorArr = new String[]{"brown", "green", "violet"};
+        System.out.println(resistorColorDuo.value(colorArr));
+
+
+    }
+}
