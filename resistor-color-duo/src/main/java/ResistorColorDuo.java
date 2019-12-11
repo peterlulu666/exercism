2a6f8070f0b5b62980381712096a27d9e63e12a5
root <root@ide.ide.9719e147-a351-4b5f-9839-94e474164604.svc.cluster.local>
2019-12-11 00:30:53 +0000

    commit

--- resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,4 +1,29 @@
+// enum Color {
+//     black,
+//     brown,
+//     red,
+//     orange,
+//     yellow,
+//     green,
+//     blue,
+//     violet,
+//     grey,
+//     white
+// }
+
 class ResistorColorDuo {
+    // int value(String[] colors) {
+    //     String code = "";
+    //     for (int i = 0; i < 2; i++) {
+    //         code = code + Color.valueOf(colors[i]).ordinal();
+
+    //     }
+
+    //     return Integer.parseInt(code);
+
+    // }
+
+
     int value(String[] colors) {
         String code = "";
         for (int i = 0; i < 2; i++) {
