4159e514c34776235e9bae1298d925ec4d6c49fd
root <root@ide.ide.9719e147-a351-4b5f-9839-94e474164604.svc.cluster.local>
2019-12-14 03:21:47 +0000

    commit

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,11 +1,47 @@
 class Hamming {
 
+    String leftStrand;
+    String rightStrand;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (this.leftStrand.isEmpty() && !this.rightStrand.isEmpty()){
+            throw new IllegalArgumentException("left strand must not be empty. ");
+
+        }
+        else if (!this.leftStrand.isEmpty() && this.rightStrand.isEmpty()){
+            throw new IllegalArgumentException("right strand must not be empty. ");
+
+        }
+        if (this.leftStrand.length() != this.rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length. ");
+        }
+
+        // leftStrand.chatAt[1] != rightStrand.chatAt[1]
+        // count ++;
+        int count = 0;
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                count++;
+
+            }
+
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        var hamming = new Hamming("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT");
+        System.out.println(hamming.getHammingDistance());
+
+
     }
 
 }
+
